91c5b85a18bbe51cd33596c2cad0475a810f5a62
dbespiatykh <d.bespiatykh@gmail.com>
2023-03-23 20:22:11 +0300

    Source version from __init__.py

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
-from setuptools import setup, find_namespace_packages
+import codecs
+import os.path
+
+from setuptools import find_namespace_packages, setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
     name="tblg",
-    version="0.1.0",
+    version=get_version("src/tblg/__init__.py"),
     description="A command-line tool to genotype Mycobacterium tuberculosis lineage from a VCF file",
     long_description=long_description,
     long_description_content_type="text/markdown",

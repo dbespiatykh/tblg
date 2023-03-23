6c889016bda0e127bdef108398ca8646def40ad3
dbespiatykh <d.bespiatykh@gmail.com>
2023-03-23 20:24:15 +0300

    Update version 0.1.0>0.1.1

--- src/tblg/__init__.py
+++ src/tblg/__init__.py
@@ -4,5 +4,5 @@ TbLG (Tuberculosis Lineage Genotyping)
 A command-line tool to genotype Mycobacterium tuberculosis lineage from a VCF file
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Dmitry Bespiatykh"

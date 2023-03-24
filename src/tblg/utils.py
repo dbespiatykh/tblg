84c33741ae65d2cd0fce8f5a3e1ff272b090b9fb
dbespiatykh <d.bespiatykh@gmail.com>
2023-03-24 13:29:20 +0300

    Refactor code; add InputOutputValidator class; move here reads and get_version functions

--- src/tblg/utils.py
+++ src/tblg/utils.py
@@ -1,6 +1,9 @@
-"""Output related functions"""
+"""Various helper functions"""
+import codecs
 import os
+import os.path
 
+import click
 import pandas as pd
 from tabulate import tabulate
 
@@ -40,10 +43,118 @@ def write_results_to_file(results, output):
 
 def print_results(results):
     """
-    Print a dataframe containing results to the console in a formatted table
+    Print a dataframe containing results to the console in a formatted table.
+
+    Args:
+    ____
+        results (pandas.DataFrame): A DataFrame containing the results to be printed.
+
+    Returns:
+    _______
+        None.
     """
     headers = ["Sample", "Level 1", "Level 2", "Level 3", "Level 4", "Level 5"]
-    if not results.empty:
-        print(tabulate(results, headers=headers, tablefmt="grid"))
+    if results.empty:
+        click.secho("ATTENTION: No lineages were called!", bold=True)
     else:
-        print("No lineages were called!")
+        table = tabulate(results, headers=headers, tablefmt="grid")
+        print(table)
+
+
+class InputOutputValidator:
+    """Class to validate input and output"""
+
+    def __init__(self, ctx, vcf_files, output):
+        """
+        Initialize InputOutputValidator.
+
+        Parameters:
+        -----------
+        ctx : click.Context
+            The click context object.
+        vcf_files : tuple
+            Tuple containing VCF files.
+        output : str
+            File path to write output to.
+        """
+        self.ctx = ctx
+        self.vcf_files = vcf_files
+        self.output = output
+
+    def validate_input(self):
+        """
+        Validate input.
+
+        Raises:
+        -------
+        click.exceptions.ClickException:
+            If no VCF files are provided and help flag is not set.
+        """
+        if not self.vcf_files and not self.ctx.params.get("help"):
+            click.secho(
+                "ATTENTION: Please provide one or more VCF files to process\n",
+                bold=True,
+            )
+            click.echo(self.ctx.get_help())
+            self.ctx.exit()
+
+    def validate_output(self):
+        """
+        Validate output.
+
+        Raises:
+        -------
+        click.exceptions.ClickException:
+            If output file doesn't have valid extension or help flag is set.
+        """
+        if self.output and not self.output.endswith((".txt", ".tsv", ".csv")):
+            click.secho(
+                "ATTENTION: Output file must have extension 'txt', 'tsv', or 'csv'\n",
+                bold=True,
+            )
+            click.echo(self.ctx.get_help())
+            self.ctx.exit()
+
+        elif self.output:
+            click.secho(f"Writing results to {self.output}", bold=True)
+
+
+def read(rel_path):
+    """
+    Read file at the specified path.
+
+    Parameters:
+    -----------
+    rel_path : str
+        Path to the file relative to this one.
+
+    Returns:
+    --------
+    str:
+        Contents of the file.
+    """
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    """
+    Extract version number from the specified file.
+
+    Parameters:
+    -----------
+    rel_path : str
+        Path to the file relative to this one.
+
+    Returns:
+    --------
+    str:
+        The version number.
+    """
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
